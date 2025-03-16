0aa8ce575366df7e662852da71e418c6d06455e4
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-16 17:30:29 +0100

    Lesson 3: Objects

---
+++ lessons/lesson3.js
@@ -0,0 +1,10 @@
+// Objects
+
+let customer = {
+    firstName: 'John',
+    lastName: 'Smith'
+}
+console.log(customer)
+console.log(customer.firstName)
+console.log(customer.lastName)
+console.log(customer['firstName'])
