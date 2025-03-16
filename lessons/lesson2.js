c764926ed5602958ad32f145bd0c651c7567bd39
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-16 16:51:03 +0100

    Lesson 2: Concatination + Interpolation

---
+++ lessons/lesson2.js
@@ -0,0 +1,7 @@
+// Concatination and Interpolation
+let price = 50
+let itemName = 'Cup'
+let messageToPrint = `The price for your ${itemName} is ${price} dollars` //interpolation
+let messageToPrint2 = 'The price for your ' + itemName + ' is ' + price + ' dollars' //concatination
+console.log(messageToPrint)
+console.log(messageToPrint2)
