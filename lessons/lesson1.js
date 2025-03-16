ee78e33c27a879c3e4268e540aab7d4ba61eb2e1
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-16 16:42:35 +0100

    Starting with variables and data types

---
+++ lessons/lesson1.js
@@ -0,0 +1,24 @@
+// Hello World
+console.log('Hello world!')
+
+// Variables
+var firstname = 'John'
+let lastName = 'Smith'
+
+var age, dateOfBirth, sex
+age = '5'
+sex = 'Male'
+console.log(age)
+age = '6'
+console.log(age)
+
+// Constant
+const occupation = 'Engineer'
+console.log(occupation)
+
+// Data types
+let middleName = 'David' //str
+let ageOfBrother = 25 //num
+let isHeMarried = false //bool
+let yearsInMarriage = null //null
+let numberOfCars = undefined //undefined
