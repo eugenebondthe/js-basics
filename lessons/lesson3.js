58fa3e095c8d83785c835ebe5c917ba795caae42
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-17 17:03:06 +0100

    Lesson 3: Completed

--- lessons/lesson3.js
+++ lessons/lesson3.js
@@ -1,10 +1,35 @@
 // Objects
-
 let customer = {
-    firstName: 'John',
-    lastName: 'Smith'
+  firstName: 'John',
+  lastName: 'Smith',
 }
+
 console.log(customer)
-console.log(customer.firstName)
-console.log(customer.lastName)
 console.log(customer['firstName'])
+console.log(customer.lastName)
+
+//Bracket notation
+customer['firstName'] = 'Eugene'
+
+//Dot notation
+customer.lastName = 'Bond'
+
+console.log(
+  `The first name was changed to ${customer.firstName} and the last name to ${customer.lastName}`,
+)
+
+//Arrays
+let car = ['Volvo', 'Tesla', 'Toyota']
+
+console.log(car[1])
+car[1] = 'BMW'
+
+console.log(car[1])
+
+let newCustomer = {
+  firstName: 'John',
+  lastName: 'Smith',
+  car: ['Volvo', 'Tesla', 'Toyota'],
+}
+
+console.log(newCustomer.car[1])
