1a6a589e3988592c73bdf1402ebb225dd6cd45f5
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-17 17:09:17 +0100

    Lesson 4: Comparison

---
+++ lessons/lesson4.js
@@ -0,0 +1,16 @@
+// Relational or comparison operators
+
+// > <- more than
+// < <- less than
+// >= <- more than equal
+// <= <- less than equal
+
+let result = 5 > 5
+
+console.log(result)
+
+// Equality operators
+
+let x = 1
+console.log(x == '1') // lose comparison
+console.log(x === '1') // strict comparison
