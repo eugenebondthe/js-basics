66fba071a2dadd8c30e353853ccc424cd4564d4e
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-03-17 17:34:36 +0100

    Lesson6: Conditional statement

---
+++ lessons/lesson6.js
@@ -0,0 +1,20 @@
+// Conditional statement
+
+if(condition){
+    //execute smthng
+} else {
+    //execute smthng
+}
+
+// If h between 6 and 12 - print 'GM'
+// if h between 12 and 18 - print 'GA'
+// Otherwise: 'GE'
+
+let h = 6
+if(h >= 6 && h < 12){
+    console.log('GM')
+} else if(h >= 12 && h < 18){
+    console.log('GA')
+} else {
+    console.log('GE')
+}
